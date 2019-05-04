3eb6690df7fa9116f9f9b3e1ff976430f88a043f
Josiah Thobejane <33384308+JosiahThobejane@users.noreply.github.com>
2019-05-04 13:36:36 +0200

    add main ino file

---
+++ Politor/Politor.ino
@@ -0,0 +1,14 @@
+const String SHIELD_ID = 003D8777;
+const String SHIELD_PAC = 12BC2CA16AE0088D;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//PART OF THE JOURNEY IS THE END - TONY START
